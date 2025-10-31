dc78b227fb8db3c9d5a8939775237bede161376e
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-31 12:41:07 +0100

    inverted_right_half_pyramid.cpp

---
+++ inverted_right_half_pyramid.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void)
+{
+    for (int i = 6; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+            std::cout << "* ";
+        std::cout << std::endl;
+    }
+
+    return (0);
+}
