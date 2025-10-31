1c53b11db5960bd6420f48a6f52e3ad1e1889ed6
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-31 14:13:58 +0100

    full_pyramid

---
+++ full_pyramid.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main(void)
+{
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 6 - i; j++)
+            std::cout << " ";
+        for (int j = 0; j < i; j++)
+            std::cout << "* ";
+        std::cout << std::endl;
+    }
+
+    return (0);
+}
